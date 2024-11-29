5e2056dbd1130d1a3127b53637f14a5bfdee9d65
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-11-29 14:27:36 +0300

    added the models in the libs folder to encript the connection with the models and the api

--- libs/prisma.js
+++ libs/prisma.js
@@ -1,7 +1,31 @@
-// libs/prisma/prismaClient.js
+import mongoose from "mongoose";
 
-import { PrismaClient } from '@prisma/client';
+const MONGODB_URI = process.env.MONGODB_URI;
 
-const prisma = new PrismaClient();
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
 
-export default prisma;
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
